975e918c3e27a3b5c78c9136b5e37771445427da
vncntchn05 <vncntchn05@gmail.com>
2024-07-19 00:11:51 -0400

    fixing graphics

--- GraphicDisplay.cc
+++ GraphicDisplay.cc
@@ -5,14 +5,14 @@ GraphicsDisplay::GraphicsDisplay(Board* subj): subj{subj} { subj->attach(this); 
 
 void GraphicsDisplay::notify() {
     int boardLen = subj->boardLength();
-    Xwindow w{50*(2 + boardLen + 10), 50*(1 + boardLen + 10)};
+    Xwindow w{50*(2 + boardLen), 50*(1 + boardLen)};
     if (subj->round1) {
         for (int i = 0; i < boardLen; i++) {
-            w.fillRectangle(0, 50*i, 100, 50, Xwindow::Green);
+            //w.fillRectangle(0, 50*i, 100, 50, Xwindow::Green);
             w.drawString(23, 50*i+25, std::to_string(8-i));
             for (int j = 0; j < boardLen; j++) {
                 if ((i + j) % 2 == 0) w.fillRectangle(50*(j+2), 50*i, 50, 50, Xwindow::White);
-                else w.fillRectangle(50*(j+2), 50*i, 50, 50, Xwindow::Blue);;
+                else w.fillRectangle(50*(j+2), 50*i, 50, 50, Xwindow::Green);;
                 for (auto p : subj->whitePieces) {
                     std::string c;
                     Type type = p->typeValue();
@@ -41,7 +41,7 @@ void GraphicsDisplay::notify() {
                 }
             }
         }
-        w.fillRectangle(0, 50*(boardLen + 1), 50*(2 + boardLen), 50, Xwindow::Green);
+        //w.fillRectangle(0, 50*(boardLen), 50*(2 + boardLen), 50, Xwindow::Green);
         for (int i = 0; i < boardLen; i++) {
             char c = ('a' + i);
             w.drawString(50*(i + 2)+23, 50*boardLen+25, std::string(1, c));
@@ -70,10 +70,10 @@ void GraphicsDisplay::notify() {
         }
         int i = m->initPos().x; int j = m->initPos().y;
         if ((i + j) % 2 == 0) w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::White);
-        else w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::Blue);
+        else w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::Green);
         i = m->finPos().x; j = m->finPos().y;
         if ((i + j) % 2 == 0) w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::White);
-        else w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::Blue);
+        else w.fillRectangle(50*i, 50*(j+2), 50, 50, Xwindow::Green);
         w.drawString(50*i, 50*(j+2), std::to_string(c));
     }
     char c;
