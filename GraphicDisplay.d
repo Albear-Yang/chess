bfaf2da1670cb6db9119e38bf31e5e14b9d11a96
Albear-yang <albert.yang727@gmail.com>
2024-07-25 16:24:36 -0400

    .

--- GraphicDisplay.d
+++ GraphicDisplay.d
@@ -1,1 +1,3 @@
-GraphicDisplay.o: GraphicDisplay.cc
+GraphicDisplay.o: GraphicDisplay.cc GraphicDisplay.h Board.h Subject.h \
+ Observer.h Piece.h Position.h Move.h Enums.h Queen.h Bishop.h Pawn.h \
+ Knight.h King.h Rook.h window.h
